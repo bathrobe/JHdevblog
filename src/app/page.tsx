cb78a882aa4436b5f19f76cc4ea3edb2328c4fcd
Joe H <hello@joeholmes.dev>
2023-11-04 16:43:39 -0400

    changed front blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,26 @@
-import PostCard from './components/PostCard'
-import getContentMetadata from './lib/getContentMetadata'
+import PostCard from "./components/PostCard";
+import getContentMetadata from "./lib/getContentMetadata";
 
 const HomePage = () => {
-  let posts = getContentMetadata('projects')
-  posts = posts.sort((a, b) => a.order - b.order)
+  let posts = getContentMetadata("projects");
+  posts = posts.sort((a, b) => a.order - b.order);
   return (
-    <div className='min-h-screen text-creamy-white'>
-      <header className='text-5xl font-bold mx-auto '>
-        <h1 className='py-2'>Hey, I&apos;m Joe.</h1>
+    <div className="min-h-screen text-creamy-white">
+      <header className="text-5xl font-bold mx-auto ">
+        <h1 className="py-2">Hey, I&apos;m Joe.</h1>
       </header>
 
-      <main className='max-w-4xl text-lg py-8 '>
-        <div className='my-6'>
-          I'm a writer, developer, and educator with a passion for simplifying
-          complex subjects. I'm currently writing and editing guides at{' '}
-          <a
-            href='https://www.sanity.io/exchange/community/joeholmes'
-            className='hover:text-green-300 underline transition-colors duration-200'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Sanity.io
-          </a>{' '}
-          and doing a little work for deeplearning.ai while looking for
-          additional work.{' '}
-          <p className='py-8'>Check out some of my recent projects below.</p>
+      <main className="max-w-4xl text-lg py-8 ">
+        <div className="my-6">
+          <p>
+            I'm a writer, developer, and educator with a passion for simplifying
+            complex subjects. I'm currently a curriculum developer specializing
+            in AI/ML at [Codecademy](https://www.codecademy.com/).
+          </p>
+          <p className="py-8">Check out some of my recent projects below.</p>
         </div>
 
-        <div className='mt-4'>
+        <div className="mt-4">
           {posts.map((post) => (
             <PostCard
               key={post.slug}
@@ -45,7 +38,7 @@ const HomePage = () => {
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
